d3cc1cc62ef2a056a626ecbd0a7e9dd177bfeece
bowtiedsimoun <133564960+bowtiedsimoun@users.noreply.github.com>
2023-05-23 21:29:22 +0800

    Update rockpaperscissors.py

--- rockpaperscissors.py
+++ rockpaperscissors.py
@@ -4,8 +4,14 @@ from random import randint
 # list of possible plays for the computer and player
 plays = ['Rock', 'Paper', 'Scissors']
 
+# MAIN CODEBASE
+# WHILE TRUE is used to ensure that looping is activated and the program 
+# continues indefinitely unless EXIT is entered by the player
 while True:
+    # the computer starts picking out its play from the PLAYS list
     computer = plays[randint(0,2)]
+    # the player enters his play manually and a TITLE function is used
+    # to ensure that the case of the COMPUTER PLAY and PLAYER PLAY is similar
     player = input("Please enter your play: ").title()
     if player == computer:
         print(f"It's a tie!")
@@ -26,5 +32,8 @@ while True:
             print(f"You lose! {computer} beats {player}")
     elif player == 'Exit':
         break
+    else:
+ 		print(f"Please enter a valid input")
+		print(f"Only choose between Rock, Paper, Scissors, or Exit")
 
 print(f"Ending Program!")
